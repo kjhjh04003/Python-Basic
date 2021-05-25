8f63869aaa68c530a368cf01d0ad6cea50c9f536
김준형 <kjhjh04003@naver.com>
2021-05-25 10:16:17 +0900

    quiz03 문제풀이

--- quiz03.py
+++ quiz03.py
@@ -37,11 +37,74 @@ def num3_summary(*args):
         minval = min(args)
         avg = total / len(args)
     return total, maxval, minval, avg
-print(num3_summary(10, 20, 30, 40, 50), type(num3_summary(10, 20, 30, 40, 50)))
 # total, maxval, minval, avg = num3_summary(80, 75, 90, 95, 85)
 # print(total, maxval, minval, avg)
 
-# if __name__ == "__main__":
+def q1():
+    while True:
+        num = input("수를 입력하세요> ")
+
+        try:
+            int(num) # 캐스팅 시도
+        except: # 캐스팅 실패
+            print("정수가 아닙니다. 다시 입력하세요")
+        else: # 정상적으로 캐스팅 완료
+            break
+
+    total = 0
+    to = int(num)
+    # for i in range(1, to + 1):
+        # if i % 3 == 0:
+            # total += i
+    lst = [i for i in range(1, to + 1) if i % 3 == 0] # 3의 배수 list
+    total = sum(lst)
+
+    print("1부터 {}까지의 3의 배수의 합 = {}".format(to, total))
+
+def q2():
+    lst_cleaned = []
+    lst = [1, 3.14, 'python', 7, 89.1, 3]
+   #  for item in lst:
+        # if isinstance(item, (int, float)):
+            # lst_cleaned.append(item)
+    lst_cleaned = [item for item in lst if isinstance(item, (int, float))]
+    print("cleaned : ", lst_cleaned)
+
+def summary(*args):
+    total = sum(args)
+    return total, max(args), min(args), total / len(args)
+
+def q3():
+    total, maxval, minval, avg = summary(80, 75, 90, 95, 85)
+    print(total, maxval, minval, avg)
+
+def q4():
+# 4. 문자열에서 ',' '.' '\n'을 제거하고 모두 대문자로 변경
+# 변경된 소스를 기반으로 단어의 빈도수를 체크
+    s = """We encourage everyone to contribute to Python. 
+    If you still have questions after reviewing the material
+    in this guide, then the Python Mentors 
+    group is available to help guide new contributors through the process."""
+
+    # 데이터 정제
+    s = s.replace(",", "").replace(".", "").replace("\n", "").upper()
+    splits = s.split()
+    print("splits:", splits)
+
+    summary = {} # 결과 dict
+    for word in splits:
+        if word in summary.keys(): #이미 키에 있으면
+            summary[word] += 1
+        else: # 키가 없으면
+            summary[word] = 1
+    for key, value in summary.items(): # (key, value) 튜플 반환
+        print("{}:{}".format(key, value))
+
+if __name__ == "__main__":
     # num1()
     # num2()
     # num3_summary()
+    # q1()
+    # q2()
+    # q3()
+    q4()
