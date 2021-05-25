4c68a347bc12b488424967c0dd3c09b8de5bfa4b
김준형 <kjhjh04003@naver.com>
2021-05-25 16:21:48 +0900

    연산자 오버로딩 연습

--- paint.py
+++ paint.py
@@ -28,7 +28,57 @@ def class_member_test():
     print("instance_count : ", p1.instance_count, # 클래스 멤버는 인스턴스에서 접근 가능
     Point.instance_count) # 인스턴스 없이도 접근 가능
 
+def test_lifecycle():
+    p1 = Point(10, 20) # 생성자 사용
+    print("instance count : ", Point.instance_count)
+
+    p2 = Point() # 기본 값 사용
+    print("instance count : ", Point.instance_count)
+
+    print(p1, p2)
+
+def test_print():
+    p1 = Point(10, 10)
+    print("p1:", p1) # __str__ 호출
+
+    # __str__ : 일반 사용자가 보기 쉽게 출력하는 포맷
+    # __repr__: 개발자가 객체 복원하기 위해 출력하는 포맷
+
+    print(repr(p1)) # __repr__ 호출
+    p2 = eval(repr(p1)) # __repr__ 출력 포맷으로 객체 복원
+    print("p2:", p2, type(p2))
+
+def arith_oper_overriding():
+    p1 = Point(10, 20)
+    p2 = Point(30, 40)
+
+    # print(dir(object)) # __eq__, __ge__ 확인
+    # Point + Point
+    print(p1 + p2) # TypeError 오류난다. +연산자가 Point and Point를 할 수 없다는 것이다.
+
+    # Point + int
+    p1 = Point(10, 20)
+    print(p1 + 10)
+
+    # int + Point
+    p1 = Point(10, 20)
+    print(10 + p1) # TypeError 오류난다. + 연산자가 int and Point를 할 수 없다는 것이다.
+
+    # # 연습문제
+    p1 = Point(10, 20)
+    p2 = Point(30, 40)
+    print(p1 - p2)
+    p1 = Point(10, 20)
+    print(p1 - 10)
+
+    p1 = Point(10, 20)
+    p2 = Point(10, 20)
+    print("p1==p2?", p1==p2)
+
 if __name__ == "__main__":
     # bound_class_mathod()
     # unbound_class_method()
-    class_member_test()
+    # class_member_test()
+    # test_lifecycle()
+    # test_print()
+    arith_oper_overriding()
--- point.py
+++ point.py
@@ -16,3 +16,52 @@ class Point:
 
     def getY(self):
         return self.y
+
+    # 생성자 : __init__
+    def __init__(self, x = 0, y = 0): # x와 y를 생성자 매개변수로 사용
+        self.x, self.y = x, y
+        Point.instance_count += 1
+
+    # 소멸자 : __del__
+    def __del__(self):
+        Point.instance_count -= 1
+
+    # 문자열 출력 포맷
+    def __str__(self):
+        return "Point x={}, y={}".format(self.x, self.y)
+
+    # 문자열 출력 포맷
+    def __repr__(self):
+        return "Point(x={}, y={})".format(self.x, self.y)
+
+    # 연산자 오버로딩 +
+    def __add__(self, other):
+        # Point + other, 연산자 뒤에 있는 것이 other로 인식
+        if isinstance(other, Point): # + Point
+            self.x += other.x
+            self.y += other.y
+        elif isinstance(other, int): # + int
+            self.x += other
+            self.y += other
+        return self
+
+    # 연산자 오버로딩 -
+    def __sub__(self, other):
+        if isinstance(other, Point):
+            self.x -= other.x
+            self.y -= other.y
+        elif isinstance(other, int):
+            self.x -= other
+            self.y -= other
+        return self
+
+    # 역이행 연산자 오버로딩
+    def __radd__(self, other):
+        if isinstance(other, int):
+            self.x += other
+            self.y += other
+        return self
+
+    # 비교 연산자 오버로딩 ==
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
