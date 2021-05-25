c8e87bc0930e556bea750264837b12dc134c1e81
김준형 <kjhjh04003@naver.com>
2021-05-25 13:33:29 +0900

    file io

--- adv_fileio.py
+++ adv_fileio.py
@@ -51,10 +51,78 @@ def copy_binary():
     f_dest.write(data)
     f_dest.close()
 
+import pickle
+def pickle_dump():
+    with open("./sample/player.bin", "wb") as f: # 자원 자동 정리, pickle을 사용하려면 바이너리 모드
+        data = {"baseball": 9}
+        pickle.dump(data, f) # 데이터 보완X
+    print("덤프 완료!")
+
+def pickle_load():
+    # 객체 역직렬화 : 2진 데이터 -> 파이썬 객체로 복원하는 작업
+    with open("./sample/player.bin", "rb") as f:
+        data = pickle.load(f)
+        print(data, type(data))
+    print("로드 완료!")
+
+def pickle_dump_multi():
+    # dump 메서드를 중복 실행하면 여러 객체를 dump 할 수 있다.
+    with open("./sample/players.bin", "wb") as f:
+        pickle.dump({"baseball": 9}, f, 1) # 프로토콜의 버전 명시 가능
+        pickle.dump({"basketball": 5}, f, pickle.HIGHEST_PROTOCOL) # 가장 최신 버전의 프로토코로 사용
+        pickle.dump({"soccor": 11}, f)
+    print("중복값 덤프 완료!")
+
+def pickle_load_multi():
+    # 파일 내부에 몇 개의 파일 객체가 저장되어 있는지 확인 어렵다
+    with open("./sample/players.bin", "rb") as f:
+        # print(pickle.load(f))
+        # print(pickle.load(f))
+        # print(pickle.load(f)) # EOF
+        # print(pickle.load(f)) # EOFERROR 발생
+
+        # EOF 에러가 발생할 때까지 loop를 돌면서 load
+        data_list = []
+        while True:
+            try:
+                data = pickle.load(f)
+            except EOFError: # 더 이상 읽을 피클이 없다.
+                break
+            data_list.append(data)
+        print(data_list)
+
+def slamdunk_read():
+    # sangbuk.csv를 한 줄 단위로 읽는다.
+    # 읽은 것 dict 시키고 list에 적재
+    # pickle에 덤프
+    players = []
+    with open("./sample/sangbuk.csv", "rt", encoding="UTF-8") as f:
+        while True:
+            line = f.readline()
+            if not line: # 읽을 데이터가 없으면
+                break
+            # 읽은 데이터를 사전화
+            # 정제
+            line = line.strip().replace(" ", "")
+            info = line.split(",")
+            print(info)
+            member = {"name": info[0], "backno": info[1], "height": info[2], "position": info[3]}
+            players.append(member)
+    print(players)
+    # pickle에 덤프
+    with open("./sample/sangbuk_players.bin", "wb") as f:
+        pickle.dump(players, f)
+    print("피클 덤프 완료!")
+
 if __name__ == "__main__":
     # write01()
     # write02()
     # read01()
     # read02()
     # read03()
-    copy_binary()
+    # copy_binary()
+    # pickle_dump()
+    # pickle_load()
+    # pickle_dump_multi()
+    # pickle_load_multi()
+    slamdunk_read()
